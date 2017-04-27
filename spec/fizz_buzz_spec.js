92ac4e35ce6ff538e8e1c2bee765d148ad74ae01
Benji <benjamincomley@gmail.com>
2017-04-27 10:48:57 +0200

    added fizz and buzz tests

--- spec/fizz_buzz_spec.js
+++ spec/fizz_buzz_spec.js
@@ -9,4 +9,12 @@ it("should return 'FizzBuzz' if number is divisible by 3 and 5", function() {
   expect(number.fizz_buzz_game(15)).toBe('FizzBuzz');
 });
 
+it("should return 'Buzz' if number is divisible by 5", function() {
+  expect(number.fizz_buzz_game(5)).toBe('Buzz');
+});
+
+it("should return 'Fizz' if number is divisible by 3", function() {
+  expect(number.fizz_buzz_game(3)).toBe('Fizz');
+});
+
 });
--- src/fizz_buzz.js
+++ src/fizz_buzz.js
@@ -4,4 +4,8 @@ function FizzBuzzGame(){
 FizzBuzzGame.prototype.fizz_buzz_game = function(num){
   if (num % 3 === 0 && num % 5 === 0)
     return ("FizzBuzz");
+  else if(num % 5 === 0)
+    return ("Buzz");
+  else if(num % 3 === 0)
+    return ("Fizz")
 }
